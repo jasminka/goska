8b1080b5e8910ba59364afa77a848d9fede6292d
Jasmina Kregar <jasmina.kregar@yahoo.com>
2014-01-14 14:54:32 +0100

    Test qgis installation.

---
+++ tests/test_qgis.py
@@ -0,0 +1,15 @@
+from qgis.core import *
+
+QgsApplication.setPrefixPath('/Applications/QGIS.app/Contents/MacOS', True)
+QgsApplication.initQgis()
+
+providers = QgsProviderRegistry.instance().providerList()
+print "Providers:"
+for provider in providers:
+    print provider
+print
+print QgsApplication.showSettings()
+
+l = QgsVectorLayer('/Users/jasmina/Dropbox/Documents/Faks/Diploma/data/obcine/Obcine.shp', 'obcine', 'ogr')
+print l.isValid()
+
